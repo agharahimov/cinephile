dbb28925f4b006ea1127fbad982be73871d7db62
agharahimov <aghasalim.agharahimov@etu.unistra.fr>
2025-10-08 10:27:30 +0400

    initial commit

---
+++ lab3/app/src/main/java/com/example/recyclerviewcontactsdemo/Contact.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerviewcontactsdemo
+
+data class Contact(
+    val id: Int,
+    val name: String,
+    val address: String,
+    val phone: String,
+    val email: String
+)
