f78495a77fbdce839c307c0393dc79587cddf101
agharahimov <aghasalim.agharahimov@etu.unistra.fr>
2025-10-08 13:00:32 +0400

    New users added

--- lab3/app/src/main/java/com/example/recyclerviewcontactsdemo/MainActivity.kt
+++ lab3/app/src/main/java/com/example/recyclerviewcontactsdemo/MainActivity.kt
@@ -8,6 +8,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import android.app.Activity
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.ItemTouchHelper
+import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
 
@@ -53,9 +55,46 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddContactActivity::class.java)
             addContactLauncher.launch(intent)
         }
+        val itemTouchHelper = ItemTouchHelper(simpleCallback)
+        itemTouchHelper.attachToRecyclerView(contactsRv)
     }
 
+    private val simpleCallback = object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN, // Directions for dragging
+        0 // Directions for swiping (0 means disabled)
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            val fromPosition = viewHolder.adapterPosition
+            val toPosition = target.adapterPosition
+
+            // Swap the items in the list
+            Collections.swap(contactList, fromPosition, toPosition)
+
+            adapter.notifyItemMoved(fromPosition, toPosition)
+
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        }
+    }
     private fun generateDummyContacts() {
+        contactList.add(Contact(1, "Anar Məmmədov", "15 Nizami küçəsi, Bakı", "050 212 34 56", "anar.m@gmail.com"))
+        contactList.add(Contact(2, "Leyla Əliyeva", "44 Neftçilər prospekti, Bakı", "055 789 12 34", "leyla.aliyeva@yahoo.com"))
+        contactList.add(Contact(3, "Fərid Həsənov", "8 Atatürk prospekti, Gəncə", "070 555 67 89", "f.hasanov@mail.ru"))
+        contactList.add(Contact(4, "Nigar Hüseynova", "102 Füzuli küçəsi, Sumqayıt", "051 321 98 76", "nigar.h@box.az"))
+        contactList.add(Contact(5, "Rəşad Quliyev", "23 Azadlıq prospekti, Bakı", "077 411 22 33", "r.guliyev@hotmail.com"))
+        contactList.add(Contact(6, "Aysel İsmayılova", "5 Heydər Əliyev prospekti, Lənkəran", "050 888 77 66", "aysel.ismayilova@gmail.com"))
+        contactList.add(Contact(7, "Orxan Abbasov", "78 İstiqlaliyyət küçəsi, Bakı", "055 123 45 67", "orkhan.a@yahoo.com"))
+        contactList.add(Contact(8, "Günay Mustafayeva", "19 Cavadxan küçəsi, Gəncə", "070 999 88 77", "gunay.m@mail.ru"))
+        contactList.add(Contact(9, "Tural Süleymanov", "33 Səməd Vurğun küçəsi, Bakı", "051 456 78 90", "tural.s@hotmail.com"))
+        contactList.add(Contact(10, "Sevinc Kərimova", "12 Sülh küçəsi, Sumqayıt", "077 345 67 89", "sevinc.k@gmail.com"))
+        contactList.add(Contact(11, "Emin Bağırov", "41 Xətai prospekti, Bakı", "050 567 11 22", "emin.bagirov@box.az"))
+        contactList.add(Contact(12, "Fidan Vəliyeva", "22 Şah İsmayıl Xətai, Gəncə", "055 234 56 78", "fidan.v@yahoo.com"))
         contactList.add(Contact(1, "Lucas Durand", "60 avenue de la République, Marseille", "0564390626", "l.durand@hotmail.com"))
         contactList.add(Contact(2, "Jean Richard", "97 rue des Ecoles, Marseille", "0543028644", "j.richard@unistra.fr"))
         contactList.add(Contact(3, "Lucas Martin", "75 rue des Ecoles, Nice", "0589700101", "l.martin@unistra.fr"))
