1c820d0cdaec148fbc66e146934767208cbd3472
agharahimov <aghasalim.agharahimov@etu.unistra.fr>
2025-11-20 13:33:21 +0400

    changes...

--- lab3/app/src/main/java/com/example/recyclerviewcontactsdemo/Contact.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recyclerviewcontactsdemo
-
-data class Contact(
-    val id: Int,
-    val name: String,
-    val address: String,
-    val phone: String,
-    val email: String
-)
